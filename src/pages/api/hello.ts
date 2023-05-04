16e3aae40dd984efde8249c52af77c7e8b5f13cc
Bobby Naluz <bnaluz9@gmail.com>
2023-05-04 13:15:14 -0700

    map error

--- src/pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
-//@ts-ignore
-export default function handler(req, res) {
-  res.status(200).json({ text: "Hello World" });
-}
