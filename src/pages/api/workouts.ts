9e201cd80584e73cf6db73c2eeaadcd5df598282
Bobby Naluz <bnaluz9@gmail.com>
2023-05-18 11:38:29 -0700

    datas moving babyyyyy

--- src/pages/api/workouts.ts
+++ src/pages/api/workouts.ts
@@ -34,10 +34,10 @@ export default async function saveWorkout(
             repCount: exercise.repCount,
             weightCount: exercise.weightCount,
             setCount: exercise.setCount,
-            userId: exercise.userId, // Use userId instead of user
+            user: { connect: { id: exercise.userId } }, // Connect to the user using userId
           })),
         },
-        userId: exercises[0].userId,
+        user: { connect: { id: exercises[0].userId } }, // Connect to the user using userId
       },
       include: {
         exercises: true,
